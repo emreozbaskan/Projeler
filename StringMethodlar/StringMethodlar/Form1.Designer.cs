c7b79fa28ead0f6abd30339eecdd46d135087e65
emreozbaskan <emreozbaskan@gmail.com>
2021-11-04 22:03:50 +0300

    String Methodlar

--- StringMethodlar/StringMethodlar/Form1.Designer.cs
+++ StringMethodlar/StringMethodlar/Form1.Designer.cs
@@ -29,13 +29,204 @@ namespace StringMethodlar
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnContains = new System.Windows.Forms.Button();
+            this.btnStartWith = new System.Windows.Forms.Button();
+            this.btnCompareTo = new System.Windows.Forms.Button();
+            this.btnEndWith = new System.Windows.Forms.Button();
+            this.btnIndexOf = new System.Windows.Forms.Button();
+            this.btnLastIndexOf = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnSubString = new System.Windows.Forms.Button();
+            this.btnToCharArray = new System.Windows.Forms.Button();
+            this.btnTrim = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(568, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnContains
+            // 
+            this.btnContains.Location = new System.Drawing.Point(12, 43);
+            this.btnContains.Name = "btnContains";
+            this.btnContains.Size = new System.Drawing.Size(148, 37);
+            this.btnContains.TabIndex = 1;
+            this.btnContains.Text = "Contains";
+            this.btnContains.UseVisualStyleBackColor = true;
+            this.btnContains.Click += new System.EventHandler(this.btnContains_Click);
+            // 
+            // btnStartWith
+            // 
+            this.btnStartWith.Location = new System.Drawing.Point(12, 86);
+            this.btnStartWith.Name = "btnStartWith";
+            this.btnStartWith.Size = new System.Drawing.Size(148, 37);
+            this.btnStartWith.TabIndex = 1;
+            this.btnStartWith.Text = "Start With";
+            this.btnStartWith.UseVisualStyleBackColor = true;
+            this.btnStartWith.Click += new System.EventHandler(this.btnStartWith_Click);
+            // 
+            // btnCompareTo
+            // 
+            this.btnCompareTo.Location = new System.Drawing.Point(12, 174);
+            this.btnCompareTo.Name = "btnCompareTo";
+            this.btnCompareTo.Size = new System.Drawing.Size(148, 37);
+            this.btnCompareTo.TabIndex = 1;
+            this.btnCompareTo.Text = "CompareTo";
+            this.btnCompareTo.UseVisualStyleBackColor = true;
+            this.btnCompareTo.Click += new System.EventHandler(this.btnCompareTo_Click);
+            // 
+            // btnEndWith
+            // 
+            this.btnEndWith.Location = new System.Drawing.Point(12, 129);
+            this.btnEndWith.Name = "btnEndWith";
+            this.btnEndWith.Size = new System.Drawing.Size(148, 37);
+            this.btnEndWith.TabIndex = 1;
+            this.btnEndWith.Text = "End With";
+            this.btnEndWith.UseVisualStyleBackColor = true;
+            this.btnEndWith.Click += new System.EventHandler(this.btnEndWith_Click);
+            // 
+            // btnIndexOf
+            // 
+            this.btnIndexOf.Location = new System.Drawing.Point(12, 217);
+            this.btnIndexOf.Name = "btnIndexOf";
+            this.btnIndexOf.Size = new System.Drawing.Size(148, 37);
+            this.btnIndexOf.TabIndex = 1;
+            this.btnIndexOf.Text = "Index Of";
+            this.btnIndexOf.UseVisualStyleBackColor = true;
+            this.btnIndexOf.Click += new System.EventHandler(this.btnIndexOf_Click);
+            // 
+            // btnLastIndexOf
+            // 
+            this.btnLastIndexOf.Location = new System.Drawing.Point(12, 260);
+            this.btnLastIndexOf.Name = "btnLastIndexOf";
+            this.btnLastIndexOf.Size = new System.Drawing.Size(148, 37);
+            this.btnLastIndexOf.TabIndex = 1;
+            this.btnLastIndexOf.Text = "Last Index Of";
+            this.btnLastIndexOf.UseVisualStyleBackColor = true;
+            this.btnLastIndexOf.Click += new System.EventHandler(this.btnLastIndexOf_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(12, 302);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(148, 37);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(166, 43);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(148, 37);
+            this.btnReplace.TabIndex = 1;
+            this.btnReplace.Text = "Replace";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Location = new System.Drawing.Point(166, 86);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(148, 37);
+            this.btnSplit.TabIndex = 1;
+            this.btnSplit.Text = "Split";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(166, 129);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(148, 37);
+            this.btnInsert.TabIndex = 1;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnSubString
+            // 
+            this.btnSubString.Location = new System.Drawing.Point(166, 174);
+            this.btnSubString.Name = "btnSubString";
+            this.btnSubString.Size = new System.Drawing.Size(148, 37);
+            this.btnSubString.TabIndex = 1;
+            this.btnSubString.Text = "SubString";
+            this.btnSubString.UseVisualStyleBackColor = true;
+            this.btnSubString.Click += new System.EventHandler(this.btnSubString_Click);
+            // 
+            // btnToCharArray
+            // 
+            this.btnToCharArray.Location = new System.Drawing.Point(166, 217);
+            this.btnToCharArray.Name = "btnToCharArray";
+            this.btnToCharArray.Size = new System.Drawing.Size(148, 37);
+            this.btnToCharArray.TabIndex = 1;
+            this.btnToCharArray.Text = "ToCharArray";
+            this.btnToCharArray.UseVisualStyleBackColor = true;
+            this.btnToCharArray.Click += new System.EventHandler(this.btnToCharArray_Click);
+            // 
+            // btnTrim
+            // 
+            this.btnTrim.Location = new System.Drawing.Point(166, 260);
+            this.btnTrim.Name = "btnTrim";
+            this.btnTrim.Size = new System.Drawing.Size(148, 37);
+            this.btnTrim.TabIndex = 1;
+            this.btnTrim.Text = "Trim";
+            this.btnTrim.UseVisualStyleBackColor = true;
+            this.btnTrim.Click += new System.EventHandler(this.btnTrim_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(592, 351);
+            this.Controls.Add(this.btnTrim);
+            this.Controls.Add(this.btnToCharArray);
+            this.Controls.Add(this.btnSubString);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnSplit);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnLastIndexOf);
+            this.Controls.Add(this.btnIndexOf);
+            this.Controls.Add(this.btnCompareTo);
+            this.Controls.Add(this.btnEndWith);
+            this.Controls.Add(this.btnStartWith);
+            this.Controls.Add(this.btnContains);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "String Methodlar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnContains;
+        private System.Windows.Forms.Button btnStartWith;
+        private System.Windows.Forms.Button btnCompareTo;
+        private System.Windows.Forms.Button btnEndWith;
+        private System.Windows.Forms.Button btnIndexOf;
+        private System.Windows.Forms.Button btnLastIndexOf;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnSubString;
+        private System.Windows.Forms.Button btnToCharArray;
+        private System.Windows.Forms.Button btnTrim;
     }
 }
 
--- StringMethodlar/StringMethodlar/Form1.cs
+++ StringMethodlar/StringMethodlar/Form1.cs
@@ -16,5 +16,218 @@ namespace StringMethodlar
         {
             InitializeComponent();
         }
+        /*
+         * Sahip oldugunuz metinsel degerlerin ozelliklerini degistirmek icin kullanilan metotlardir. Ornegin, kelimeleri parcalamak, eklemek, harflerin yerlerini degistirmek, bosluk temizlemek vs...
+         */
+
+        private void btnContains_Click(object sender, EventArgs e)
+        {
+            //Contains => Verilen string ifadeyi çağırılan metin içerisinde arar
+            string metin = textBox1.Text;
+            bool result = metin.Contains("çok"); //ekrandan girilen metin içerisinde çok ifadesi geçiyor mu?
+            if (result == true)
+            {
+                MessageBox.Show("çok ifadesi metin içerisinde geçiyor");
+            }
+            else
+            {
+                MessageBox.Show("ifade metin içerisinde geçmiyor");
+            }
+        }
+
+        private void btnStartWith_Click(object sender, EventArgs e)
+        {
+            //StartsWith => Kelimenizin parametrede gonderdiginiz degerle baslayip baslamadigini size "bool" olarak dondurur...
+            //start with default olarak camel case yani büyük küçük duyarlılığı vardır.
+            string metin = textBox1.Text;
+            bool result = metin.StartsWith("bil");
+            MessageBox.Show(result == true ? "ifade bil ile başlıyor" : "ifade bil ile başlamıyor");
+
+        }
+
+        private void btnCompareTo_Click(object sender, EventArgs e)
+        {
+            //Metodu kullandiginiz string degerle metoda verdiginiz parametredeki string degeri sozluk mantigiyla karsilastirir... Eger sozlukte ayni lokasyonlardasa 0, parametredeki deger, ana degerimizden sozlukte onceyse - 1; sonraysa 1 degerini dondurur...
+
+
+            string metin = textBox1.Text;
+            int sonuc = metin.CompareTo("bilgeadam");
+            string mesaj = "";
+            switch (sonuc)
+            {
+                case -1:
+                    {
+                        mesaj = "Kelime diğer kelime ile eşleşmektedir.";
+                        break;
+                    }
+                case 0:
+                    {
+                        mesaj = "Kelime diğer kelime ile eşleşmektedir";
+                        break;
+                    }
+                case 1:
+                    {
+                        mesaj = "Kelime diğer kelime ile eşleşmemektedir.";
+                        break;
+                    }
+            }
+
+            MessageBox.Show(mesaj);
+
+            //int result1 = "5".CompareTo("6");
+            //int result2 = "6".CompareTo("5");
+            //int result3 = "5".CompareTo("5");
+
+
+        }
+
+        private void btnEndWith_Click(object sender, EventArgs e)
+        {
+            //EndsWith => Kelimenizin parametrede gonderdiginiz degerle bitip bitmedigini size "bool" olarak dondurur...
+
+            string metin = textBox1.Text;
+            bool result = metin.EndsWith("dam");
+            MessageBox.Show(result == true ? "metin dam ile bitiyor" : "kelimenin sonu dam ile bitmiyor");
+
+        }
+
+        private void btnIndexOf_Click(object sender, EventArgs e)
+        {
+            //IndexOf => Dizilerde gordugumuz metodun ayni prensibleriyle calisir. Eger harf varsa harfin indexini, yoksa -1 degerini, harf birden fazla var ise ilk harfin indexini dondurur...
+            //string metin = textBox1.Text;
+            //int index = metin.IndexOf("çok");
+
+
+            //MessageBox.Show(index.ToString());
+
+            //metin içerisinde birden fazla geçiyor ise
+            string metin = textBox1.Text;
+            int kackere = 0;
+            int startIndex = 0;
+            while (true)
+            {
+                startIndex = metin.IndexOf("çok", startIndex);
+                if (startIndex == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    kackere++;
+                    startIndex++;
+                }
+            }
+
+            MessageBox.Show(kackere.ToString());
+        }
+
+        private void btnLastIndexOf_Click(object sender, EventArgs e)
+        {
+            //LastIndexOf => Dizilerde gordugumuz metodun ayni prensibiyle calisir. Eger harf varsa harfin indexini, yoksa - 1 degerini, harf birden fazla varsa son harfin indexini dondurur... arama işlemine son indexden başlayarak döner
+            string metin = textBox1.Text;
+            int sonuc = metin.LastIndexOf("çok");
+            MessageBox.Show(sonuc.ToString());
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            //string içerisinden belli bir kısmı silmek için kullanılır.
+            //metni değiştirmez yeni bir kopyasını döner
+            string metin = textBox1.Text;
+            string metinKopya = metin.Remove(5);
+            MessageBox.Show(metin);
+            MessageBox.Show(metinKopya);
+
+            //2. Kullanım
+            //kaç index başla kaç eleman sil
+            string metinKopya2 = metin.Remove(5, 3);
+            MessageBox.Show(metinKopya2);
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            // Replace => Metniniz icerisindeki bir karakteri ya da bir bolumu yeni bir karakter - bolum - ile degistirmenize olanak saglar..
+            //hangi metin veya string ifde üstünden çağırıldığında içerisinde birden fazla da geçse hepsini dönüştürür.
+            string metin = textBox1.Text;
+            //string metinKopya = metin.Replace("adam", "kadın");
+            //MessageBox.Show(metinKopya);
+
+            string[] musteriler = { "Emre", "Yalın", "Havva" };
+            decimal[] faturalar = { 157.65m, 199.89m, 385.85m };
+            for (int i = 0; i < musteriler.Length; i++)
+            {
+                string metinKopya1 = metin.Replace("[Adi]", musteriler[i]);
+                metinKopya1 = metinKopya1.Replace("[Tutar]", faturalar[i].ToString());
+                MessageBox.Show(metinKopya1);
+            }
+
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            //Split => Metninizi ozel bir karakterden birden fazla parcaya ayirmaniza olanak saglayan metottur. Ayni anda birden fazla ozel karakter de verebilirsiniz...
+            string metin = textBox1.Text;
+            //string[] metinDizisi = metin.Split(' ');
+            string[] metinDizisi = metin.Split(' ', ';', ',');
+
+            for (int i = 0; i < metinDizisi.Length; i++)
+            {
+                MessageBox.Show(metinDizisi[i]);
+            }
+
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            //Insert => Bir metinsel degerin herhangi bir pozisyonuna (index) yeni bir degeri ilistirmek istiyorsaniz bu metodu kullanabilirsiniz. İlk parameterde kacinci indexten sonra ekleyecegini, ikinci parametrede ise hangi metni ekleyecegini sorar...
+            string metin = textBox1.Text;
+            string metinKopya = metin.Insert(3, "güzel");
+
+
+            MessageBox.Show(metinKopya);
+        }
+
+        private void btnSubString_Click(object sender, EventArgs e)
+        {
+            //Substring (1.Kullanım) => Metninizden, parametrede gonderdiginiz indexten baslayarak, geri kalan kismi cekip almaniza olanak saglar...
+            //Substring (2.Kullanım) => Metninizden, ilk parametrede verdiginiz indexten baslayip, ikinci parametrede verdiginiz deger kadarlik kismi cekip almaniza olanak saglar..
+
+            string metin = textBox1.Text;
+            string metinKopya = metin.Substring(5);
+            string metinKopya2 = metin.Substring(7, 4);
+            MessageBox.Show(metinKopya);
+            MessageBox.Show(metinKopya2);
+        }
+
+        private void btnToCharArray_Click(object sender, EventArgs e)
+        {
+            //ToCharArray => Stringinizi charlarina ayirarak size char[] olarak teslim eder...
+            string metin = textBox1.Text;
+            char[] charDizi = metin.ToCharArray();
+            for (int i = 0; i < charDizi.Length; i++)
+            {
+                MessageBox.Show(charDizi[i].ToString());
+            }
+        }
+
+        private void btnTrim_Click(object sender, EventArgs e)
+        {
+            //Trim => Metninizin saginda ya da solundaki tum bosluklari temizlemenize olanak saglayan metottur...
+            //metin içerisindeki boşluklara dokunmaz
+            string metin = textBox1.Text;
+            MessageBox.Show(metin.Trim());
+        }
+
+        //Ödev
+        //kendi extension şeklinde insert metodunuzu yazınız
     }
 }
+
+//public static class CustomExtension
+//{
+//    public static string CustomInsert(this string s, int startIndex, string caracters)
+//    {
+
+//    }
+//}
