0abd10ee04e775d29dae657e6aae4d7661df5fa1
emreozbaskan <emreozbaskan@gmail.com>
2021-11-04 19:22:32 +0300

    yeni proje sunucuya atılıyor

---
+++ Parse_Example/Parse_Example/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parse_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tam Sayısal Veri Tipleri için dönüşümler
+            //Console.Write("Lütfen bir sayı giriniz." + byte.MinValue + " - " + byte.MaxValue + " :");
+            //byte byte_Degisken = byte.Parse(Console.ReadLine());
+
+            //Console.Write("Lütfen bir sayı giriniz.." + short.MinValue + " " + short.MaxValue + " :");
+            //short short_Degisken = short.Parse(Console.ReadLine());
+            //Console.WriteLine("Değişkenin girilen değeri=>" + short_Degisken);
+
+            //Console.Write("Lütfen bir sayı giriniz.." + int.MinValue + " " + int.MaxValue + " :");
+            //int int_Degisken = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Değişkenin girilen değeri=>" + int_Degisken);
+
+            //Console.Write("Lütfen bir sayı giriniz.." + long.MinValue + " " + long.MaxValue + " :");
+            //long long_Degisken = long.Parse(Console.ReadLine());
+            //Console.WriteLine("Değişkenin girilen değeri=>" + long_Degisken);
+
+            //Ondalıklı Sayılar
+            //Console.Write("Lütfen bir sayı giriniz.." + float.MinValue + " " + float.MaxValue + " :");
+            //float float_Degisken = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Değişkenin girilen değeri=>" + float_Degisken);
+
+            //Console.Write("Lütfen bir sayı giriniz.." + double.MinValue + " " + double.MaxValue + " :");
+            //double double_Degisken = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Değişkenin girilen değeri=>" + double_Degisken);
+
+            //Console.Write("Lütfen bir sayı giriniz.." + decimal.MinValue + " " + decimal.MaxValue + " :");
+            //decimal decimal_Degisken = decimal.Parse(Console.ReadLine());
+            //Console.WriteLine("Değişkenin girilen değeri=>" + decimal_Degisken);
+
+
+            ////Metinsel Değişkenler
+            ////short donusecek_degisken = 125;
+            //Console.Write("Lütfen bir metin giriniz...");
+            //string string_degisken = Console.ReadLine();/*donusecek_degisken.ToString();*/
+            ////Console.WriteLine(string_degisken);
+
+            //Console.WriteLine("Para transferini onaylıyormusunuz => (E - H)");
+            ////Console.ReadKey() => Bir tuşa basılmasını bekler Console.ReadKey().KeyChar basın tuşu verir..
+            //char char_Degisken = Console.ReadKey().KeyChar;
+            //Console.WriteLine("Ekrana Basılan Değer=>"+char_Degisken);
+
+            //Mantıksal Değişken
+            //Console.WriteLine("İşlemi Onaylıyormusunuz (Onaylıyorsanız true Onaylamıyorsanız false basın)");
+            //bool bool_degisken = bool.Parse(Console.ReadLine());
+            //Console.WriteLine("Basılan Değer => " + bool_degisken);
+
+            //O anki tarih saati verir.
+            //Console.WriteLine(DateTime.Now.ToString());
+            //Console.WriteLine(DateTime.Now.Year - 1989);
+
+            Console.ReadKey();
+        }
+    }
+}
