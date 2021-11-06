e0f687e11aa67ad23f32b4674a9a0519d0be08c8
emreozbaskan <emreozbaskan@gmail.com>
2021-11-06 10:18:26 +0300

    Tarihde hazır methodlar gönderildi

--- MathMetodlar/MathMetodlar/Form1.Designer.cs
+++ MathMetodlar/MathMetodlar/Form1.Designer.cs
@@ -29,13 +29,75 @@ namespace MathMetodlar
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnDatetime = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnTimeSpan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDatetime
+            // 
+            this.btnDatetime.Location = new System.Drawing.Point(12, 12);
+            this.btnDatetime.Name = "btnDatetime";
+            this.btnDatetime.Size = new System.Drawing.Size(171, 41);
+            this.btnDatetime.TabIndex = 0;
+            this.btnDatetime.Text = "DateTime";
+            this.btnDatetime.UseVisualStyleBackColor = true;
+            this.btnDatetime.Click += new System.EventHandler(this.btnDatetime_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(234, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(234, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // btnTimeSpan
+            // 
+            this.btnTimeSpan.Location = new System.Drawing.Point(12, 59);
+            this.btnTimeSpan.Name = "btnTimeSpan";
+            this.btnTimeSpan.Size = new System.Drawing.Size(171, 41);
+            this.btnTimeSpan.TabIndex = 0;
+            this.btnTimeSpan.Text = "TimeSpan";
+            this.btnTimeSpan.UseVisualStyleBackColor = true;
+            this.btnTimeSpan.Click += new System.EventHandler(this.btnTimeSpan_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(780, 470);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTimeSpan);
+            this.Controls.Add(this.btnDatetime);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnDatetime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnTimeSpan;
     }
 }
 
--- MathMetodlar/MathMetodlar/Form1.cs
+++ MathMetodlar/MathMetodlar/Form1.cs
@@ -16,5 +16,70 @@ namespace MathMetodlar
         {
             InitializeComponent();
         }
+
+        private void btnDatetime_Click(object sender, EventArgs e)
+        {
+            //Uygulamayı kullanan user bilgisinin culture ayarı
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("tr-TR");
+            MessageBox.Show(DateTime.Now.ToString());
+
+            //o andki tarih ve saat bilgisini döner
+            label1.Text = DateTime.Now.ToString();
+
+            //Suanki günün Tarihini alma
+            label1.Text = DateTime.Now.ToShortDateString();
+            label2.Text = DateTime.Now.ToLongDateString();
+
+            //Parça şeklinde alma
+            //Year=> Yıl
+            //Month=> ay
+            //Day =>Gün
+            //Hour=> Saat
+            //Minute => Dakika
+            //Second => Saniye
+            //Milisecond
+            label1.Text = DateTime.Now.Year + "/" + DateTime.Now.Month + "/" + DateTime.Now.Day + " T " + DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second + " " + DateTime.Now.Millisecond;
+
+            //Artik Yil
+            bool IsLeapYear = DateTime.IsLeapYear(1989);
+            MessageBox.Show(IsLeapYear == true ? "Şubat 29 çeker" : "Şubat 28 Çeker");
+
+            //Bulunduğunuz yılın artıl yılmı bilgisini bu şekilde kontrol edebilirsiniz
+            //DateTime.IsLeapYear(DateTime.Now.Year);
+
+            //Haftanın gün bilgisi
+            DayOfWeek dayOfWeek = DateTime.Now.DayOfWeek;
+            MessageBox.Show("Haftanın günü=>" + dayOfWeek.ToString());
+
+            //Yılın kaçıncı günü
+            int dayOfYear = DateTime.Now.DayOfYear;
+            MessageBox.Show("Yılın kaçıncı günü=>" + dayOfYear.ToString());
+
+
+            label1.Text = DateTime.UtcNow.ToString();
+            label2.Text = DateTime.Now.ToString();
+
+        }
+
+        private void btnTimeSpan_Click(object sender, EventArgs e)
+        {
+            //kendiniz bir tarih saat nesnesi oluşmak istediğiniz bu şekilde oluşturabilirsiniz
+            DateTime date1 = new DateTime(1988, 1, 1);
+            DateTime date = new DateTime(1990, 6, 1, 12, 12, 12);
+            DateTime bugun = DateTime.Now;
+
+            TimeSpan fark = bugun - date;
+            //fark.TotalDays //toplam gün
+            //fark.TotalHours //toplam Saat
+            //fark.TotalMinutes 
+            //fark.TotalSeconds
+            //fark.TotalMilliseconds
+
+            
+            MessageBox.Show("İki tarih arası toplam gün=>" + fark.Days);
+
+
+
+        }
     }
 }
